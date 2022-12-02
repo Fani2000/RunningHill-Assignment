7f12ae04df0d6be073bd1de62e92fb3f5201cb87
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-12-02 10:57:45 +0200

    Fixed the main front-end site content, only left with backend to setup and get data to use in the main ui

--- frontend/src/app/components/drag-section/drag-section.component.ts
+++ frontend/src/app/components/drag-section/drag-section.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import {CdkDragDrop, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import {CdkDragDrop, transferArrayItem, moveItemInArray} from '@angular/cdk/drag-drop';
+import { Word } from 'src/app/models/Word';
+import { WordsServiceService } from 'src/app/services/words-service.service';
 
 @Component({
   selector: 'app-drag-section',
   templateUrl: './drag-section.component.html',
   styleUrls: ['./drag-section.component.css']
 })
-export class DragSectionComponent {
-  words: string[] = ["The", "Was", "Thief", "Police", "Caught", ".", "A"]
-  sentence: string[] = []
-  correctSentence: string = "The Police Caught A Thief."
 
-  drop(event: CdkDragDrop<string[]>){
-   transferArrayItem(event.previousContainer.data,event.container.data, event.previousIndex, event.currentIndex) 
+export class DragSectionComponent implements OnInit {
+  constructor(private wordsServices: WordsServiceService) {}
+
+  ngOnInit(): void {
+    console.log(this.wordsServices.getWords())
+  }
+  words: Word[] = []
+  constructedSentence: Word[] = []
+  correctSentence: string = ""
+
+  drop(event: CdkDragDrop<Word[]>){
+    console.log(event.container, event.container.data.length)
+   transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.container.data.length) 
+  }
+
+  onSubmitSentence(){
+    // console.log(this.sentence.join(" "))
+    const newSentence = this.constructedSentence.map(w => w.word).join(" ")
+    this.wordsServices.submitSentence(newSentence)
   }
 }
--- frontend/src/app/services/words-service.service.ts
+++ frontend/src/app/services/words-service.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { Word } from '../models/Word';
+import { Subject, Observable, of } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WordsServiceService {
+  private words: Word[] = []; 
+  private sentences: Subject<string[]> = new Subject<string[]>();
+  private _sentences: string[] = []
 
-  constructor() { }
+  constructor() {}
+
+  getWords() {
+    return this.words;
+  }
+
+  submitSentence(sentence: string) {
+    this._sentences.push(sentence)
+    this.sentences.next([...this._sentences])
+    console.log(this._sentences);
+  }
 }
